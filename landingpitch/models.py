ee1c3068bf381c2ffc144f520997570cd07589af
aldyahsn <aldy.ahsandin@gmail.com>
2024-09-22 15:52:22 +0700

    add homepage

--- landingpitch/models.py
+++ landingpitch/models.py
@@ -2,19 +2,66 @@ from django.db import models
 
 from wagtail.models import Page
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
 from wagtail.search import index
 
+
 # Create your models here.
+class HomePage(Page):
+    # add the Hero section of HomePage:
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+        help_text="Homepage image",
+    )
+    hero_text = models.CharField(
+        blank=True, max_length=255, help_text="Write an introduction for the site"
+    )
+    hero_cta = models.CharField(
+        blank=True,
+        verbose_name="Hero CTA",
+        max_length=255,
+        help_text="Text to display on Call to Action",
+    )
+    hero_cta_link = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+        verbose_name="Hero CTA link",
+        help_text="Choose a page to link to for the Call to Action",
+    )
+
+    body = RichTextField(blank=True)
+
+    template = 'landingpitch/pages/homepage.html'
+
+    # modify your content_panels:
+    content_panels = Page.content_panels + [
+        MultiFieldPanel(
+            [
+                FieldPanel("image"),
+                FieldPanel("hero_text"),
+                FieldPanel("hero_cta"),
+                FieldPanel("hero_cta_link"),
+            ],
+            heading="Hero section",
+        ),
+        FieldPanel("body"),
+    ]
+
+
 class BlogPage(Page):
     intro = RichTextField(blank=True)
 
-    template = 'landingpitch/pages/blogpage.html'
-    subpage_types = ['landingpitch.PostPage']
+    template = "landingpitch/pages/blogpage.html"
+    subpage_types = ["landingpitch.PostPage"]
 
-    content_panels = Page.content_panels + [
-        FieldPanel('intro')
-    ]
+    content_panels = Page.content_panels + [FieldPanel("intro")]
 
 
 class PostPage(Page):
@@ -22,17 +69,17 @@ class PostPage(Page):
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
 
-    template = 'landingpitch/pages/postpage.html'
-    parent_page_types = ['landingpitch.BlogPage']
+    template = "landingpitch/pages/postpage.html"
+    parent_page_types = ["landingpitch.BlogPage"]
     subpage_types = []
 
     search_fields = Page.search_fields + [
-        index.SearchField('intro'),
-        index.SearchField('body'),
+        index.SearchField("intro"),
+        index.SearchField("body"),
     ]
 
     content_panels = Page.content_panels + [
-        FieldPanel('date'),
-        FieldPanel('intro'),
-        FieldPanel('body'),
+        FieldPanel("date"),
+        FieldPanel("intro"),
+        FieldPanel("body"),
     ]
