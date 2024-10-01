021fce5c86fb56f420e619c639d443ac7b26a5f3
aldyahsn <aldy.ahsandin@gmail.com>
2024-10-01 11:46:07 +0700

    add menu

--- landingpitch/models.py
+++ landingpitch/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from wagtail.models import Page
 
 # Create your models here.
+class BlankPage(Page):
+    template = 'landingpitch/blankpage.html'
+
+class BlogPage(Page):
+    template = 'landingpitch/blogpage.html'
+
+class PostPage(Page):
+    template = 'landingpitch/postpage.html'
+
