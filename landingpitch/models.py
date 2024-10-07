465f4e41d060ea20c03e47b9a254e79fbd259f8a
aldyahsn <aldy.ahsandin@gmail.com>
2024-10-07 17:21:00 +0700

    add blog page

--- landingpitch/models.py
+++ landingpitch/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from django.conf import settings
 from django.db import models
 from wagtail.admin.panels import FieldPanel
 from wagtail.fields import RichTextField
@@ -10,15 +11,23 @@ class BlankPage(Page):
     template = 'landingpitch/blankpage.html'
 
 class BlogPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro')
+    ]
+
     template = 'landingpitch/blogpage.html'
 
 class PostPage(Page):
+    short = RichTextField(blank=True)
     body = RichTextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     content_panels = Page.content_panels + [
         FieldPanel('body'),
+        FieldPanel('short'),
     ]
 
     template = 'landingpitch/postpage.html'
